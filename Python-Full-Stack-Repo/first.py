2992197f9371cdd55c76d0ec46ac59c46dc52cca
Anuj Shelke <anujshelke18@gmail.com>
2026-09-18 01:26:39 +0530

    Code to print Factor of a number

--- Python-Full-Stack-Repo/first.py
+++ Python-Full-Stack-Repo/first.py
@@ -1,16 +1,13 @@
-#Code to Check if a number is a perfect Number 
-i =1
-sum = 0
-num = int(input("Enter number to check"))
-while(i <=num/2):
+#Code to calculate factor 
+num = int(input("Enter number to check factors of "))
+i = 1
+print(f"Factor of Number {num} is :")
+while(i<=num/2):
     if(num%i==0):
-        sum = i+sum
-    i+=1
-print(sum)
-if(sum == num):
-    print("The number is a perfect Number ")
-else: 
-    print("The number is not a perfect Number ")
+        print(i)
+        i+=1
+
+
 
     
 
