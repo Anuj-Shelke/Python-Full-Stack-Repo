67002b045c64442ec93451b6801645de93c6d879
Anuj Shelke <anujshelke18@gmail.com>
2026-09-18 01:58:02 +0530

    Code to print this Star Pattern Arrow Form

--- Python-Full-Stack-Repo/first.py
+++ Python-Full-Stack-Repo/first.py
@@ -1,8 +1,19 @@
-#Code to print multiplication table of a given number 
-num = int(input("Enter number to print table of "))
-print("The multiplication Table of number is : ")
-for i in range(1,11):
-    print(f"{num} X {i} = {num*i}")
+# #Code to print this Star Pattern 
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+# *****
+# ****
+# ***
+# **
+# *
+for row in range(1,6):
+    print("*"*row)
+for row in range(6,0,-1):
+    print("*"*row)
     
 
 
