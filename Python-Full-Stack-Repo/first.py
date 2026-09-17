e36b4dceb013d8be623034e17ce56052b7061e24
Anuj Shelke <anujshelke18@gmail.com>
2026-09-18 01:48:55 +0530

    Code to print multiplication table of a given number

--- Python-Full-Stack-Repo/first.py
+++ Python-Full-Stack-Repo/first.py
@@ -1,13 +1,9 @@
-#Code to calculate factorial of a number 
-num = int(input("Enter number to Print factorial of  "))
-i = 1
-fact = 1
-temp = 0 
-for i in range(1,num+1):
-    fact =fact*i
-   
-print(fact)
-
+#Code to print multiplication table of a given number 
+num = int(input("Enter number to print table of "))
+print("The multiplication Table of number is : ")
+for i in range(1,11):
+    print(f"{num} X {i} = {num*i}")
+    
 
 
     
