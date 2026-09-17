6fe61b7d688f6a36acae17d91144ce9b1f88ab40
Anuj Shelke <anujshelke18@gmail.com>
2026-09-18 01:10:27 +0530

    Code to print Fabonacci Series

--- Python-Full-Stack-Repo/first.py
+++ Python-Full-Stack-Repo/first.py
@@ -1,15 +1,14 @@
-#Code to check if a number is a pallidrome of not 
-mod = 0
-div = 0 
-num = 85
+#Code to Print fabonnaci Series using loop 
+i = 0; 
+first = 0
+second = 1
+next = 0
+while(i<10):
+     print(first)
+     next = first+second
+     first = second 
+     second = next 
+    
+     i+=1
+     
 
-while(num > 0 ): 
-    div = num%10
-    mod = div+mod*10
-    num = num//10
-print(mod)
-
-if(mod == num):
-    print("The number is a Pallindrome ")
-else: 
-    print("The number is not a Pallindrome")
