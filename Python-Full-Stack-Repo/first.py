bf45b898cb7b6eed31f18e97b235ad8395f661d7
Anuj Shelke <anujshelke18@gmail.com>
2026-09-18 01:45:35 +0530

    Code to print Factorial of a number

--- Python-Full-Stack-Repo/first.py
+++ Python-Full-Stack-Repo/first.py
@@ -1,11 +1,12 @@
-#Code to calculate factor 
-num = int(input("Enter number to check factors of "))
+#Code to calculate factorial of a number 
+num = int(input("Enter number to Print factorial of  "))
 i = 1
-print(f"Factor of Number {num} is :")
-while(i<=num/2):
-    if(num%i==0):
-        print(i)
-        i+=1
+fact = 1
+temp = 0 
+for i in range(1,num+1):
+    fact =fact*i
+   
+print(fact)
 
 
 
