c5097136336eb9c7ebcbcc1175858330e227a3cb
Anuj Shelke <anujshelke18@gmail.com>
2026-09-18 00:52:53 +0530

    #Code to check if a number is a pallidrome of not

--- Python-Full-Stack-Repo/first.py
+++ Python-Full-Stack-Repo/first.py
@@ -1,12 +1,15 @@
-#Code to Reverse a Number 
-num = 21
-div = 0
+#Code to check if a number is a pallidrome of not 
 mod = 0
+div = 0 
+num = 85
 
-while(num> 0):
-    div = num%10     
+while(num > 0 ): 
+    div = num%10
     mod = div+mod*10
     num = num//10
-   
-print(mod)    
-    
+print(mod)
+
+if(mod == num):
+    print("The number is a Pallindrome ")
+else: 
+    print("The number is not a Pallindrome")
