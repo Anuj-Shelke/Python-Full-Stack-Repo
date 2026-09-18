b7bf08e4e198770514f67d3a09787ea63b81c7f0
Anuj Shelke <anujshelke18@gmail.com>
2026-09-18 16:01:25 +0530

    Code to print the given pattern

--- Python-Full-Stack-Repo/first.py
+++ Python-Full-Stack-Repo/first.py
@@ -1,22 +1,16 @@
-#Code to check if a number is a Strong number 
-#Number is a strong number if sum of its factor is number itself eg 145 
-num = 145
-temp = num
-fact = 1
-sum =0 
-while(num >0):
-    mod  = num%10
-    fact = 1
-    for i in range(mod,0,-1): 
-      fact = fact*i
-      
-    sum = sum+fact
-    num = num//10
+#Code to print This pattern using both while loop 
+# * * * * 
+# * * * * 
+# * * * * 
+# * * * * 
+row = 0  
+coln = 0 
 
-
-if(sum == temp):
-   print("The number is a strong number ")
-else : 
-   print("not a strong num")
-  
-
+while(row<4): 
+   print("*",end=" ")
+   coln = 1 
+   while(coln<4):
+      print("*",end=" ")
+      coln+=1
+   print()
+   row+=1
